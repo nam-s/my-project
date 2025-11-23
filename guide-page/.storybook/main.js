dc555c6c3b8530b0f22e80d24b319080111ff2b4
Nams <sungs0415@naver.com>
2025-11-23 14:56:48 +0900

    [남승헌] 스토리북 세팅

---
+++ guide-page/.storybook/main.js
@@ -0,0 +1,16 @@
+/** @type { import('@storybook/vue3-vite').StorybookConfig } */
+const config = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@chromatic-com/storybook',
+    '@storybook/addon-docs',
+    // '@storybook/addon-onboarding',
+    '@storybook/addon-a11y',
+    // '@storybook/addon-vitest',
+  ],
+  framework: {
+    name: '@storybook/vue3-vite',
+    options: {},
+  },
+}
+export default config
